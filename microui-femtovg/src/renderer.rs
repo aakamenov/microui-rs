5dc6ffa58507f3e61ee0cefd9328f33a86d76c1e
aakamenov <asparuhkamenov@gmail.com>
2023-02-07 00:27:34 +0200

    Update femtovg version which allows us to update winit as well.

--- microui-femtovg/src/renderer.rs
+++ microui-femtovg/src/renderer.rs
@@ -1,3 +1,5 @@
+use std::num::NonZeroU32;
+
 use microui_app::{
     MicrouiRenderer,
     microui::{
@@ -11,18 +13,29 @@ use microui_app::{
     }
 };
 
-use glutin::{ContextBuilder, ContextWrapper, PossiblyCurrent};
 use femtovg::{
     Canvas, TextContext, Baseline, Align, Paint, Path,
     FontId as FemtovgFontId, Color as FemtovgColor,
     renderer::OpenGl
 };
 
+use glutin::{
+    config::ConfigTemplateBuilder,
+    context::{ContextApi, ContextAttributesBuilder, PossiblyCurrentContext},
+    display::GetGlDisplay,
+    surface::{SurfaceAttributesBuilder, Surface, WindowSurface},
+    prelude::*
+};
+use glutin_winit::DisplayBuilder;
+use raw_window_handle::HasRawWindowHandle;
+
 const DEFAULT_FONT: &[u8] = include_bytes!("../../fonts/ProggyClean.ttf");
 const FONT_SIZE_PT: f32 = 16.0;
 
 pub struct Renderer {
-    ctx: ContextWrapper<PossiblyCurrent, Window>,
+    window: Window,
+    ctx: PossiblyCurrentContext,
+    surface: Surface<WindowSurface>,
     canvas: Canvas<OpenGl>,
     text_context: TextContext,
     font_id: FemtovgFontId,
@@ -42,25 +55,84 @@ impl MicrouiRenderer for Renderer {
         window_builder: WindowBuilder,
         event_loop: &EventLoop<()>
     ) -> Self {
-        let ctx_builder = ContextBuilder::new()
-            .with_vsync(false)
-            .build_windowed(window_builder, &event_loop)
+        let template = ConfigTemplateBuilder::new()
+
+            .prefer_hardware_accelerated(Some(true))
+            .with_alpha_size(8);
+
+        let display_builder = DisplayBuilder::new().with_window_builder(Some(window_builder));
+
+        let (window, gl_config) = display_builder.build(&event_loop, template, |configs| {
+            // Find the config with the least number of samples, so our triangle will be smooth.
+            configs.reduce(|accum, config| {
+                let transparency_check = config.supports_transparency().unwrap_or(false) &
+                    !accum.supports_transparency().unwrap_or(false);
+
+                if transparency_check || config.num_samples() < accum.num_samples() {
+                    config
+                } else {
+                    accum
+                }
+            }).unwrap()
+        }).unwrap();
+
+        let window = window.unwrap();
+        let window_handle = Some(window.raw_window_handle());
+        let display = gl_config.display();
+
+        let context_attributes = ContextAttributesBuilder::new()
+            .build(window_handle);
+
+        let fallback_context_attributes = ContextAttributesBuilder::new()
+            .with_context_api(ContextApi::Gles(None))
+            .build(window_handle);
+
+        let mut not_current_gl_context = Some(unsafe {
+            display.create_context(&gl_config, &context_attributes)
+                .unwrap_or_else(|_| {
+                    display.create_context(
+                        &gl_config,
+                        &fallback_context_attributes
+                    ).expect("Couldn't create OpenGL context.")
+                })
+        });
+
+        let (width, height): (u32, u32) = window.inner_size().into();
+        let window_handle = window.raw_window_handle();
+
+        let attrs = SurfaceAttributesBuilder::<WindowSurface>::new().build(
+            window_handle,
+            NonZeroU32::new(width).unwrap(),
+            NonZeroU32::new(height).unwrap(),
+        );
+
+        let surface = unsafe {
+            gl_config.display()
+                .create_window_surface(&gl_config, &attrs)
+                .unwrap()
+        };
+
+        let ctx = not_current_gl_context.take()
+            .unwrap()
+            .make_current(&surface)
             .unwrap();
 
-        let ctx = unsafe { ctx_builder.make_current().unwrap() };
         let renderer = unsafe {
-            OpenGl::new_from_function(|s| ctx.get_proc_address(s) as *const _)
-        }.unwrap();
+            OpenGl::new_from_function_cstr(|s|
+                display.get_proc_address(s) as *const _
+            )
+        }.expect("Couldn't create renderer.");
 
         let text_context = TextContext::default();
         let mut canvas = Canvas::new_with_text_context(renderer, text_context.clone()).unwrap();
         let font_id = canvas.add_font_mem(DEFAULT_FONT).unwrap();
 
-        let size = ctx.window().inner_size();
-        canvas.set_size(size.width, size.height, ctx.window().scale_factor() as f32);
+        canvas.set_size(width, height, window.scale_factor() as f32);
 
         let renderer = Renderer {
+            window,
             ctx,
+            surface,
             canvas,
             text_context,
             font_id,
@@ -72,13 +144,18 @@ impl MicrouiRenderer for Renderer {
 
     #[inline]
     fn resize(&mut self, size: PhysicalSize<u32>, scale_factor: f64) {
-        self.ctx.resize(size);
+        self.surface.resize(
+            &self.ctx,
+            size.width.try_into().unwrap(),
+            size.height.try_into().unwrap()
+        );
+
         self.canvas.set_size(size.width, size.height, scale_factor as f32);
     }
 
     #[inline]
     fn window(&self) -> &Window {
-        self.ctx.window()
+        &self.window
     }
 
     #[inline]
@@ -87,13 +164,13 @@ impl MicrouiRenderer for Renderer {
             self.clear_color = FemtovgColor::rgba(color.r, color.g, color.b, color.a);
         }
 
-        let size = self.ctx.window().inner_size();
+        let size = self.window.inner_size();
         self.canvas.clear_rect(0, 0, size.width, size.height, self.clear_color);
 
         ctx.handle_commands(self);
 
         self.canvas.flush();
-        self.ctx.swap_buffers().unwrap();
+        self.surface.swap_buffers(&self.ctx).unwrap();
     }
 
     #[inline]
@@ -120,7 +197,7 @@ impl CommandHandler for Renderer {
             FemtovgColor::rgba(color.r, color.g, color.b, color.a)
         );
 
-        self.canvas.fill_path(&mut path, paint);
+        self.canvas.fill_path(&mut path, &paint);
     }
 
     #[inline]
@@ -139,7 +216,7 @@ impl CommandHandler for Renderer {
                 FemtovgColor::rgba(color.r, color.g, color.b, color.a)
             );
 
-        self.canvas.fill_text(pos.x as f32, pos.y as f32, text, paint).unwrap();
+        self.canvas.fill_text(pos.x as f32, pos.y as f32, text, &paint).unwrap();
     }
 
     #[inline]
@@ -167,12 +244,12 @@ impl CommandHandler for Renderer {
                 FemtovgColor::rgba(color.r, color.g, color.b, color.a)
             );
             
-        let metrics = self.canvas.measure_text(0., 0., text, paint).unwrap();
+        let metrics = self.canvas.measure_text(0., 0., text, &paint).unwrap();
 
         let x = rect.x as f32 + (rect.w as f32 - metrics.width()) / 2.;
         let y = rect.y as f32 + (rect.h as f32 - metrics.height()) / 2.;
 
-        self.canvas.fill_text(x, y, text, paint).unwrap();
+        self.canvas.fill_text(x, y, text, &paint).unwrap();
     }
 }
 
@@ -183,7 +260,7 @@ impl TextSizeHandler for FemtovgTextSizeHandler {
             .with_font(&[self.font_id])
             .with_font_size(FONT_SIZE_PT);
 
-        let metrics = self.ctx.measure_text(0., 0., text, paint).unwrap();
+        let metrics = self.ctx.measure_text(0., 0., text, &paint).unwrap();
 
         metrics.width() as i32
     }
@@ -194,7 +271,7 @@ impl TextSizeHandler for FemtovgTextSizeHandler {
             .with_font(&[self.font_id])
             .with_font_size(FONT_SIZE_PT);
 
-        let metrics = self.ctx.measure_font(paint).unwrap();
+        let metrics = self.ctx.measure_font(&paint).unwrap();
         
         metrics.height() as i32
     }
